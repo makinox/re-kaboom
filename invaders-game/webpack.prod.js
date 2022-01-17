e53a1773ded3ca5d074e807f99fa16b3e1058f52
Jesus Bossa <x@jesusbossa.dev>
2022-01-16 20:18:21 -0500

    chore: secure

--- invaders-game/webpack.prod.js
+++
@@ -1,8 +0,0 @@
-const common = require('./webpack.common.js');
-const { merge } = require('webpack-merge');
-
-module.exports = merge(common, {
-  target: ['web', 'es2017'],
-  mode: 'production',
-  devtool: 'source-map',
-});
